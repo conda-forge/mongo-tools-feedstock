dd1006463be9a7007f6843e8c82c0f6af5bb2bd7
Patrick Sodré <psodre@gmail.com>
2018-12-12 04:44:27 +0000

    Use git_url and git_rev instead of git archive
      - In git-hub those two should be essentially equivalent as far as reproducibility.
      - It simplifies the build.sh code
      - It is the standard way to build GO binaries outside of conda
      - It was done as a suggestion from @jakirkham during packaging of singularity.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,3 @@
-# Git Initialize
-# Apps tend to use git info to create version string
-git init
-git config --local user.email "conda@conda-forge.github.io"
-git config --local user.name "conda-forge"
-
-echo $PKG_VERSION >> .conda_version
-git add .conda_version
-git commit -m "conda build of $PKG_NAME-r$PKG_VERSION"
-git tag -a -m "conda build of $PKG_NAME-r$PKG_VERSION" r${PKG_VERSION}
-
 # Use upstream gopath setup
 . ./set_gopath.sh
 
