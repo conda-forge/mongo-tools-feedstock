bcc8e34484f835e20055d2ac299cec1e9787e33b
Daniel Nachun <daniel.nachun@gmail.com>
2025-02-02 14:21:35 -0800

    use different variable name

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,9 +2,9 @@
 
 set bin_names=bsondump mongodump mongorestore mongoimport mongoexport mongostat mongotop mongofiles
 
-( for %%bin_name in (%bin_names%) do (
-        go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%%bin_name.exe -ldflags="-X main.VersionStr=%PKG_VERSION% -s -w" .\%%bin_name\main\%%bin_name.go || goto :error
-        go-licenses save .\%%bin_name --save_path=license-files_%%bin_name || goto :error
+( for %%a in (%bin_names%) do (
+        go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%%a.exe -ldflags="-X main.VersionStr=%PKG_VERSION% -s -w" .\%%a\main\%%a.go || goto :error
+        go-licenses save .\%%a --save_path=license-files_%%a || goto :error
     )
 )
 
